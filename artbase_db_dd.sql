f57690481de52dc92e14125c986922767058baac
kanwisck <55958734+kanwisck@users.noreply.github.com>
2020-05-18 10:34:24 -0700

    Added sample data to relationship tables

--- artbase_db_dd.sql
+++ artbase_db_dd.sql
@@ -45,26 +45,15 @@ CREATE TABLE `Users_Events` (
   CONSTRAINT FOREIGN KEY (`eventID`) REFERENCES Events (`eventID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Artists Table*/
-
-DROP TABLE IF EXISTS `Artists`;
-    
-CREATE TABLE `Artists` ( 
-    `artistID` int AUTO_INCREMENT PRIMARY KEY,
-    `username` varchar(25) NOT NULL UNIQUE,
-    `password` varchar(25) NOT NULL,
-    `firstName` varchar(25) NOT NULL,
-    `lastName` varchar(25) NOT NULL,
-    `email` varchar(255) NOT NULL,
-    `birthdate` date,
-    CONSTRAINT `full_name` UNIQUE (`firstName`, `lastName`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-INSERT INTO Artists (username, password, firstName, lastName, email, birthdate) VALUES 
-    ('SteveJ45', 'password1', 'Steve', 'Johnson', 'SteveJ45@gmail.com', '1975-05-22'),
-    ('KeatG', 'password2', 'George', 'Keating', 'georgekeat@gmail.com', '1976-09-13'),
-    ('W W', 'asd123', 'Melissa', 'Baker', 'baker125@yahoo.com', '1995-10-05');
-
+INSERT INTO Users_Events (userID, eventID) VALUES
+	((SELECT Users.userID FROM Users WHERE Users.username='caspersv'),
+     (SELECT Events.eventID FROM Events WHERE Events.name='Plaza Art Fair')),
+    ((SELECT Users.userID FROM Users WHERE Users.username='caspersv'),
+     (SELECT Events.eventID FROM Events WHERE Events.name='Bayou City Art Festival')),
+    ((SELECT Users.userID FROM Users WHERE Users.username='qmoore30'),
+     (SELECT Events.eventID FROM Events WHERE Events.name='Art Fair Nashville')),
+    ((SELECT Users.userID FROM Users WHERE Users.username='alorTay1'),
+     (SELECT Events.eventID FROM Events WHERE Events.name='Plaza Art Fair'));
 
 /*Artists Table*/
 
@@ -93,7 +82,7 @@ DROP TABLE IF EXISTS `Artworks`;
 CREATE TABLE `Artworks` ( 
     `artworkID` int(11) AUTO_INCREMENT,
     `artistID` int(11),
-    `title` varchar(25) NOT NULL,
+    `title` varchar(255) NOT NULL,
     `medium` varchar(25) NOT NULL,
     `material` varchar(25) NOT NULL,
     `description` varchar(255) NOT NULL,
@@ -119,3 +108,23 @@ CREATE TABLE `Artworks_Events` (
     CONSTRAINT FOREIGN KEY (`artworkID`) REFERENCES Artworks(`artworkID`),
     CONSTRAINT FOREIGN KEY (`eventID`) REFERENCES Events(`eventID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO Artworks_Events (artworkID, eventID) VALUES
+    (
+        (SELECT Artworks.artworkID FROM Artworks
+	    LEFT JOIN Artists ON Artworks.artistID=Artists.artistID
+        WHERE Artists.username = 'W W'),
+        (SELECT Events.eventID FROM Events WHERE Events.name='Plaza Art Fair')
+    ),
+    (
+        (SELECT Artworks.artworkID FROM Artworks
+	    LEFT JOIN Artists ON Artworks.artistID=Artists.artistID
+        WHERE Artists.username = 'SteveJ45'),
+        (SELECT Events.eventID FROM Events WHERE Events.name='Art Fair Nashville')
+    ),
+    (
+        (SELECT Artworks.artworkID FROM Artworks
+	    LEFT JOIN Artists ON Artworks.artistID=Artists.artistID
+        WHERE Artists.username = 'KeatG'),
+        (SELECT Events.eventID FROM Events WHERE Events.name='Bayou City Art Festival')
+    );
