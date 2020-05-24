869da9334e190fac58a22a14714ccd423d450adb
kanwisck <55958734+kanwisck@users.noreply.github.com>
2020-05-24 11:44:50 -0700

    Initial implementation of sessions

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ var express = require('express');
 var fs = require('fs');
 var mysql = require('./dbcon.js');
 var app = express();
+var session = require('express-session');
 var bodyParser = require('body-parser');
 var handlebars = require('express-handlebars');
 var artworkData = require('./fakedb.json'); // Temporary fake database for testing
 var path = require('path');
-//Sets default directory
+
+
 app.use('/static', express.static('public/js'));
 app.use(express.static(__dirname + '/public'));
-
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitializd: true
+}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.engine('handlebars', handlebars({extname: 'handlebars', 
                                     defaultLayout: 'main', 
@@ -19,17 +26,50 @@ app.set('view engine', 'handlebars');
 app.set('mysql', mysql);
 app.set('port', 7791);
 
-app.get('/',function(req,res,next){
-  var context = {}; 
-  context.type = "artist/user";
-  context.artworkData = artworkData;
-  res.render('home', context);
+app.get('/', function(req,res) {
+  if(req.session.loggedin) {
+    res.render('home');
+  }
+  res.render('login');
+})
+
+app.get('/home',function(req,res,){
+  if (req.session.loggedin) {
+    var context = {}; 
+    context.artworkData = artworkData;
+    context.userInfo = req.session.userInfo;
+    res.render('home', context);
+  } else {
+    // could create handlebars page for failed authentication 
+    // instead of code below and render it
+    res.write('<h1>Please login First.</h1>');
+    res.end('<a href=' + '/' + '>Login</a>');
+  }
+});
+
+app.post('/authu', function(req,res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  if (username && password) {
+    mysql.pool.query('SELECT * FROM Users WHERE username=? AND password=?', [username, password], function(error, results, fields) {
+      if (error) {
+        res.send('Incorrect username and/or password');
+      } else {
+        req.session.loggedin = true;
+        req.session.userInfo = results[0];
+        res.redirect('/home');
+      }
+      res.end();
+    });
+  } else {
+    res.send('Please enter username and password')
+    res.end();
+  }
 });
 
 app.get('/upload', function(req,res,next) {
   var callbackCount = 0;
   var context = {};
-  context.type = "artist/user";
   mysql.pool.query("SELECT name FROM Events ORDER BY name ASC", function(error, results, fields){
     if(error){
       res.write(JSON.stringify(error));
@@ -45,36 +85,7 @@ app.get('/upload', function(req,res,next) {
     }
   }
 });
-/*
-Inactive for step 3 draft
-app.post('/upload', function(req,res,next) {
-  //pushes entered parameters to fakedb
-  artworkData.push(
-    {
-      title: req.body.title,
-      medium: req.body.medium,
-      materials: req.body.materials,
-      description: req.body.description,
-      rating: 0,
-      url: req.body.url
-    }
-  );
-  fs.writeFile(__dirname + '/fakedb.json', JSON.stringify(artworkData, 2, 2),
-    function(err) {
-      if(!err) {
-        res.status(200).send();
-      }
-      else {
-        res.status(500).send("Couldn't write to fakedb");
-      }
-    }
-  );
-});
 
-app.get('/upload2', function(req,res,next) {
-  res.render('upload');
-})
-*/
 app.get('/user-signup',function(req,res,next){
   var context = {};
   res.render('user-signup', context);
@@ -137,3 +148,9 @@ app.use(function(err, req, res, next){
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.')
 });
+
+function setVals(results) {
+  userResults = results;
+  console.log(userResults);
+  return userResults;
+}
---
+++ userlogin.js
@@ -0,0 +1,7 @@
+module.exports = function() {
+    var express = require('express');
+    var router = express.Router();
+    var bodyParser = require('body-parser');
+    var urlencodedParser = bodyParser.urlencoded({extended: false});
+    
+}
